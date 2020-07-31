2c45df14903071db63ff5272c50bafff6ca761fc
Joseph Banks <joseph@josephbanks.me>
2020-07-31 13:57:39 +0100

    Load OAuth2 blueprint

--- shortener/__init__.py
+++ shortener/__init__.py
@@ -1,19 +1,23 @@
+import secrets
 from os import environ
 
 from flask import Flask, redirect
 from werkzeug.exceptions import NotFound
 
 from shortener.blueprints.api import api
+from shortener.blueprints.oauth2 import oauth2
 from shortener.models import db, ShortURL
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = secrets.token_hex(64)
 
 db.init_app(app)
 
 app.register_blueprint(api, url_prefix="/api")
+app.register_blueprint(oauth2, url_prefix="/oauth2")
 
 
 @app.route("/")
