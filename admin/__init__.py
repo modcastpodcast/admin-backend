961b94db5c2bf3e6735412bcd3723d5073e3e3d8
Joseph Banks <joseph@josephbanks.me>
2020-08-10 02:06:38 +0100

    Update middleware

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,4 +1,3 @@
-import secrets
 from os import environ
 
 from starlette.applications import Starlette
@@ -11,8 +10,9 @@ from admin.models import db
 from admin.route_manager import create_route_map
 
 middleware = [
-    Middleware(SessionMiddleware, secret_key=secrets.token_hex(32)),
-    Middleware(CORSMiddleware,
+    Middleware(SessionMiddleware, secret_key=environ.get("SECRET_KEY")),
+    Middleware(
+        CORSMiddleware,
         allow_origins=[
             "https://admin.modpod.live",
             "http://127.0.0.1:3000"
@@ -22,8 +22,12 @@ middleware = [
             "Content-Type"
         ],
         allow_methods=["*"]
+    ),
+    Middleware(
+        DatabaseMiddleware,
+        db=db,
+        database_url=environ.get("DATABASE_URL")
     )
 ]
 
 app = Starlette(routes=create_route_map(), middleware=middleware)
-app.add_middleware(DatabaseMiddleware, db=db, database_url=environ.get("DATABASE_URL"))
